1b23407bb652e96ba19e68db65aa2ef1b370636e
unknown <thapabiwash2002@gmail.com>
2026-03-29 20:49:50 +0545

    fix gitignore and clean project

--- .next/static/chunks/src_app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__917f460a._.css",
-  "static/chunks/node_modules_cd3a59d0._.js",
-  "static/chunks/src_4b2b7327._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_page_tsx_4bfc1643._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_2ac24900._.js",
-  "static/chunks/node_modules_e8ce0d2a._.js"
-],
-    source: "dynamic"
-});
